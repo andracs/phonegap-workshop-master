4c8120bbdc099a8497122322f0d024a2996a954d
András Ács <acsandras@gmail.com>
2014-04-11 21:53:37 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
+	findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
 
 	showAlert: function (message, title) {
 		if (navigator.notification) {
@@ -39,9 +32,9 @@ var app = {
     initialize: function() {
     	var self = this;
         this.store = new MemoryStore(function() {
-        	self.showAlert('Store Initialized', 'Info');
         	self.renderHomeView();
-    	});
+        	this.homeTpl = Handlebars.compile($("#home-tpl").html());
+			this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());    	});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
